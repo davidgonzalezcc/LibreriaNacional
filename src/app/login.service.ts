7064ae8059f374b6eb033ba22addc81b2a3412b6
David <78158460+davidgonzalezcc@users.noreply.github.com>
2022-10-10 22:59:39 -0500

    encriptado

--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -1,12 +1,22 @@
 import { HttpClient } from '@angular/common/http';
-import {Observable} from "rxjs";
+
 import { Injectable } from '@angular/core';
+import * as CryptoJS from 'crypto-js';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+//Sha-256
+  secretKey = "secretito";
 
   constructor(private http:HttpClient) { }
 
+  encriptar(value : string) : string{
+    return CryptoJS.AES.encrypt(value, this.secretKey.trim()).toString();
+  }
+
+  desencriptar(textToDecrypt : string){
+    return CryptoJS.AES.decrypt(textToDecrypt, this.secretKey.trim()).toString(CryptoJS.enc.Utf8);
+  }
 }
